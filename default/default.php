812ca197a3741c1af2a986ee70eb194e0cc10c4f
Witrold <theo.buchon3@gmail.com>
2021-12-07 21:53:51 +0100

    Initial commit

---
+++ default/default.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>StartGame</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <script src='main.js'></script>
+    <link rel="stylesheet" href="styledelault.css">
+</head>
+<body>
+    
+    <center> <nav class="menu-nav">
+        <ul>
+            <li class="btn">
+                <a href="/start/7LiP593bRXhEtW.html">
+                    1ére énigme
+                </a> 
+            </li>
+        </ul>
+    </nav> </center> 
+
+</body>
+</html>
