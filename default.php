abc20c7e7e3ad2d936109797751ccf1c7d797a65
Witrold <theo.buchon3@gmail.com>
2021-12-08 18:35:50 +0100

    bug #2

---
+++ default.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>StartGame</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <script src='main.js'></script>
+    <link rel="stylesheet" href="styledefault.css">
+</head>
+<body>
+    
+    <h1 class="titre">
+        TheÉnigmeFr
+    </h1>
+    
+    <h1 class="lesenigme">
+        Les énigme :
+    </h1>
+
+    <center> <nav class="menu-nav">
+        <ul>
+            <li class="btn">
+                <a href="Maintenance/maintenance.html">
+                    1ére énigme
+                </a> 
+            </li>
+        </ul>
+    </nav> </center> 
+
+</body>
+</html>
